95d05a78678fddf873ae7cfd848fbace2009a97e
ckcks12 <wyoducc@gmail.com>
2015-08-07 04:53:53 +0900

    create sending thread, fix videowriter and convert to webm with ffmpeg

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@
 using namespace std;
 using namespace cv;  
 
-int main() {
+int main() {  
     ofstream				output;	
 	cv::VideoCapture		cam;
 	cv::VideoWriter			writer;
@@ -23,30 +23,40 @@ int main() {
 	const char*					window_name_main; 
 	const char*					video_name;
 	const char*					output_name;
+	const char*					video_webm_name;
+	string						ffmpeg_convert_to_webm;
 	int						i, j; 
     Human                                   humanCoord;
     Scalar                                  humanColor;
 	int						hist_r, hist_g, hist_b;
-
+	CircularQueue<cv::Mat>	video_queue(100); 
 	BackgroundSubtractorMOG2        bg;
 	vector< vector<Point> >	contours; 
 	cv::Mat					contours_image;
 	postClass				post_data;
 	bool					do_output;
+	bool					isDanger;
+	bool					isUploading;
+	bool					is_send_data;
 
 	// initialize
 	waitKey_exit = 'q';
-	waitKey_delay = 1;
+	waitKey_delay = 300;
 	window_name_main = "asdf"; 
 	video_name = "temp.avi";
+	video_webm_name = "temp.webm";
+	ffmpeg_convert_to_webm = "ffmpeg -i ";
+	ffmpeg_convert_to_webm += video_name;
+	ffmpeg_convert_to_webm += " -vcodec libvpx -y ";
+	ffmpeg_convert_to_webm += video_webm_name;
 	output_name = "data.txt"; 
 	cam.open(0); 
 	cv::namedWindow( window_name_main );
-	writer = cv::VideoWriter(video_name, CV_FOURCC('M', 'J', 'P', 'G'),
-							15.0, Size(VIDEO_WIDTH, VIDEO_HEIGHT),
-							true);
 	do_output = false;
-	if( do_output ) output.open(output_name, 'w');
+	isDanger = false;
+	isUploading = false;
+	is_send_data = false;
+	if( do_output ) output.open(output_name, 'w'); 
 	
 	// manual start 
     while( true ) {
@@ -56,10 +66,44 @@ int main() {
         cv::imshow( window_name_main, frame );
     } 
 
+	// send thread create 
+	thread sendToServerCaution_thread([&]() {
+		string rtn;
+		while (true) {
+			if (!isUploading && is_send_data ) {
+				rtn = sendToServerCaution(post_data.giveMeJson().c_str());
+				if (rtn == "1")
+					isDanger = true;
+				is_send_data = false;
+			}
+		}
+	});
+	thread sendToServerVideo_thread([&]() {
+		string rtn;
+		while (true) {
+			if (isDanger) {
+				isUploading = true;
+				// saveVideo
+				writer = cv::VideoWriter(video_name, CV_FOURCC('M', 'P', '4', '2'), 3.0, Size(VIDEO_WIDTH, VIDEO_HEIGHT), true);
+				while (video_queue.size())
+					writer << video_queue.dequeue();
+				system(ffmpeg_convert_to_webm.c_str());
+				rtn = sendToServerVideo(video_webm_name);
+				if (rtn == "1")
+					isUploading = isDanger = false;
+			}
+		}
+	});
+
+
 	// main loop
     while( true ) {
         // get frame from cam
-        cam >> frame;
+        cam >> frame; 
+
+		// enqueue
+		if (!isDanger)
+			video_queue.enqueue(frame);
             
 		// background subtraction
 		bg.operator()(frame, fore, 0);
@@ -73,9 +117,26 @@ int main() {
         erode(fore, fore, Mat());
         dilate(fore, fore, Mat());
         dilate(fore, fore, Mat());
-
-		// filling
-		cout << "filling start" << endl;
+		
+		//save test 
+		/*
+		int key = 0;
+		imshow(window_name_main, frame);
+		if (!isDanger)
+			video_queue.enqueue(frame.clone());   
+		key = cv::waitKey(waitKey_delay); 
+		if (key == 'q') {
+			break;
+		}
+		else if (key == 32) {
+			if (!isDanger)
+				isDanger = true;   
+		}
+		cout << video_queue.size() << endl;
+		continue;
+		*/
+		 
+		// filling 
         contours_image = fore.clone(); 
 		findContours(contours_image, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE); 
 		for (int i = 0; i < contours.size(); i++)
@@ -88,18 +149,16 @@ int main() {
 				drawContours(fore, contours, i, Scalar(0), CV_FILLED); 
 			else 
 				drawContours(fore, contours, i, Scalar(255), CV_FILLED); 
-		}
-		cout << "filling success" << endl;
+		} 
 EXIT_FILLING:
-		
+
 		memset(&humanColor.val, 0, sizeof(humanColor.val));
 		memset(&humanCoord, 0, sizeof(Human));
 
         blobs = getBlobs(&fore);
         getBlobMat(&frame,&fore, blobs, &blobs_image, humanColor);  
 
-		if (blobs_image.size() > 0) {
-			cout << "blob found" << endl;
+		if (blobs_image.size() > 0) { 
 			// skeleton
 			if (starSkeleton(blobs_image[0], blobs_image[0], humanCoord, 20)) {
 				// show!
@@ -107,23 +166,23 @@ EXIT_FILLING:
 
 				// post data
 					// make data
-				post_data.push_head(humanCoord.head);
-				post_data.push_left_hand(humanCoord.larm);
-				post_data.push_right_hand(humanCoord.rarm);
-				post_data.push_center(humanCoord.center);
-				post_data.push_hist(humanColor.val[2], humanColor.val[1], humanColor.val[0]);
-				post_data.push_upperHand(isHarzardous(humanCoord));
+					post_data.push_head(humanCoord.head);
+					post_data.push_left_hand(humanCoord.larm);
+					post_data.push_right_hand(humanCoord.rarm);
+					post_data.push_center(humanCoord.center);
+					post_data.push_hist(humanColor.val[2], humanColor.val[1], humanColor.val[0]);
+					//post_data.push_upperHand(isHarzardous(humanCoord));	
 				// console print
-				cout << post_data.giveMeJson() << endl;
+					//cout << post_data.giveMeJson() << endl;
 				// output
-				if (do_output) output << post_data.giveMeJson() << endl;
-				// sending.... ? 
+					if (do_output) output << post_data.giveMeJson() << endl;
+				// sending
+					is_send_data = true;
 			}
 		}
 
-        if (cv::waitKey(waitKey_delay) == waitKey_exit ) {
-                break; 
-        } 
+		if (cv::waitKey(waitKey_delay) == waitKey_exit)
+			break; 
     } 
 	 
 	system("pause");
